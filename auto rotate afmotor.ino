2e535069a0a0ee07c58ec05f24d0f2f4af3e1033
juniorroboticsclub <101139640+juniorroboticsclub@users.noreply.github.com>
2022-03-08 19:15:41 +0600

    Create auto rotate afmotor.ino

---
+++ auto rotate afmotor.ino
@@ -0,0 +1,108 @@
+#include <AFMotor.h>
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+
+#define echo A2    //Echo pin
+#define trigger A3 //Trigger pin
+
+int distance_L, distance_F = 30, distance_R;
+long distance;
+int set = 20;
+
+int Speed = 90;
+   
+
+void setup(){ // put your setup code here, to run once
+
+  motor1.setSpeed(Speed);
+  motor2.setSpeed(Speed);
+  motor3.setSpeed(Speed);
+  motor4.setSpeed(Speed);
+
+pinMode(echo, INPUT );// declare ultrasonic sensor Echo pin as input
+pinMode(trigger, OUTPUT); // declare ultrasonic sensor Trigger pin as Output  
+
+
+
+Serial.begin(9600);
+
+
+
+}
+
+
+void loop(){  
+
+//===============================================================================
+//                          Obstacle Avoiding Control
+//===============================================================================     
+ distance_F = Ultrasonic_read();
+ Serial.print("S=");Serial.println(distance_F);
+  if (distance_F < set)
+  {
+    turnRight();
+  }else{
+    forword();
+    }
+  
+}
+
+delay(10);
+}
+
+
+
+
+//**********************Ultrasonic_read****************************
+long Ultrasonic_read(){
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  distance = pulseIn (echo, HIGH);
+  return distance / 29 / 2;
+}
+
+
+void forword(){  //forword
+
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+    motor3.run(FORWARD);
+    motor4.run(FORWARD);
+ 
+}
+
+void backword(){ //backword
+
+    motor1.run(BACKWARD);
+    motor2.run(BACKWARD);
+    motor3.run(BACKWARD);
+    motor4.run(BACKWARD);
+ 
+}
+
+void turnRight(){ //turnRight
+
+    motor1.run(FORWARD);
+    motor2.run(FORWARD);
+    motor3.run(BACKWARD);
+    motor4.run(BACKWARD);
+  
+}
+void turnLeft(){ //turnRight
+
+    motor1.run(BACKWARD);
+    motor2.run(BACKWARD);
+    motor3.run(FORWARD);
+    motor4.run(FORWARD);
+}
+void Stop(){ //stop
+
+    motor1.run(RELEASE);
+    motor2.run(RELEASE);
+    motor3.run(RELEASE);
+    motor4.run(RELEASE);
+}
